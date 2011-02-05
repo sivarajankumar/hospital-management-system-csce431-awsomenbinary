8d3e349544e7397bd3bc3ae4337a9c76c7db67e9
djnemec <djnemec@6555da7d-b502-80bd-02a4-f5b87940fd8c>
2011-02-05 20:25:31 +0000

    Added SQLConnector class. Will eventually hold methods for inserting/retrieving necessary data from the database.

---
+++ SQLConnector/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SQLConnector{
+
+    class Program{
+
+        static void Main(string[] args){
+            SQLConnector c = new SQLConnector("192.168.1.102", "djnemec-hosp", "djnemec", "csce431");
+            c.connect();
+
+            if (c.userExists("dan") && c.verifyUser("dan", "pass")){
+                Console.WriteLine("User exists!");
+            }
+            
+            if(!c.userExists("test1")){
+                c.addUser("test1", "ppp");
+                Console.WriteLine("Error adding user");
+            }
+            if (!c.userExists("test1")) {
+                Console.WriteLine("Error adding user");
+            }
+            c.disconnect();
+
+        }
+    }
+}
