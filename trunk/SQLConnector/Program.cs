84b334c0e895b66aa5bfd2ebc514a23d5ff6a945
djnemec <djnemec@6555da7d-b502-80bd-02a4-f5b87940fd8c>
2011-02-08 16:06:35 +0000

    Adding SQL dll.

--- trunk/SQLConnector/Program.cs
+++ trunk/SQLConnector/Program.cs
@@ -5,7 +5,7 @@ namespace SQLConnector{
     class Program{
 
         static void Main(string[] args){
-            SQLConnector c = new SQLConnector("192.168.1.102", "djnemec-hosp", "djnemec", "csce431");
+            SQLConnector c = new SQLConnector("database-new.cse.tamu.edu", "djnemec-hosp", "djnemec", "csce431");
             c.connect();
 
             if (c.userExists("dan") && c.verifyUser("dan", "pass")){
