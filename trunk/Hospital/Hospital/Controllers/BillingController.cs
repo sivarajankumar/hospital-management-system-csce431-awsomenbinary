098103c02a0a12d74ae86adb1f9b71a3fcf6c17e
airsoftsoldrecn9@neo.tamu.edu <airsoftsoldrecn9@neo.tamu.edu@6555da7d-b502-80bd-02a4-f5b87940fd8c>
2011-04-19 15:02:07 +0000

    Billing Update 1

---
+++ trunk/Hospital/Hospital/Controllers/BillingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Hospital.Controllers
+{
+    public class BillingController : GenericController
+    {
+        //
+        // GET: /Billing/
+
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return redirectToPortal();
+            }
+            return View();
+        }
+
+        public ActionResult EditBilling()
+        {
+            return View();
+        }
+
+        public ActionResult ShowBilling()
+        {
+            return View();
+        }
+    }
+}
