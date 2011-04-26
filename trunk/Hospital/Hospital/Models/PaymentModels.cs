b14aa0507b5b0f3bd546e942f3ab214c502e8467
kfarinella19 <kfarinella19@6555da7d-b502-80bd-02a4-f5b87940fd8c>
2011-04-26 15:23:32 +0000

    working on Payment stuff

--- trunk/Hospital/Hospital/Models/PaymentModels.cs
+++ trunk/Hospital/Hospital/Models/PaymentModels.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MySql.Data.MySqlClient;
+using Hospital.Providers;
 
 namespace Hospital.Models
 {
-    public class PaymentModels
+
+    #region models
+    public class PaymentRecords
     {
+        public int UserID { get; set; }
+        public double PayRate { get; set; }
+        public int Hours { get; set; }
+        public double SurgeryRate { get; set; }
+        public int SurgeryHours { get; set; }
+
     }
+
+    #endregion
+
 }
