6f25b16316641c66300d776184b58fe50621d68c
airsoftsoldrecn9 <airsoftsoldrecn9@neo.tamu.edu>
2011-04-26 05:54:34 +0000

    Billing System changes III

--- Hospital/Hospital/Controllers/BillingController.cs
+++ Hospital/Hospital/Controllers/BillingController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Hospital.Models;
 using System.Web.Security;
+using Microsoft.CSharp;
 
 namespace Hospital.Controllers
 {
     public class BillingController : GenericController
     {
+        BillingServices svc = new BillingServices();
         //
         // GET: /Billing/
 
@@ -23,7 +26,19 @@ namespace Hospital.Controllers
 
         public ActionResult EditBilling()
         {
-            return View();
+            if (User.IsInRole("Doctor") || User.IsInRole("Nurse") || User.IsInRole("Pharmacist"))
+            {
+                svc.EditBilling(id, true);
+            }
+            else
+
+
+                return RedirectToAction("Index");
+        }
+        public ActionResult Edit(String doctor, String specialization, DateTime time)
+        {
+           
+            return RedirectToAction("Index");
         }
 
         public ActionResult ShowBilling()
--- Hospital/Hospital/Models/BillingModel.cs
+++ Hospital/Hospital/Models/BillingModel.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MySql.Data.MySqlClient;
+using Hospital.Providers;
 
 namespace Hospital.Models
 {
+    
        #region models
     public class BillingRecords
     {
@@ -18,10 +26,29 @@ namespace Hospital.Models
        #endregion
     public class BillingServices
     {
-        public void makePayment(int id)
+        private readonly BillingProvider _provider;
+
+        public BillingProviderService() : this(null)
         {
-            _provider.cancelAppointment(id);
+
         }
+        public BillingProviderService(BillingProvider provider)
+        {
+            _provider = provider ?? new BillingProvider();
+        }
+        public void editBillingStatement(int user_id, DateTime date, double amt, string service_item )
+        {
+            _provider.addBill(user_id, date, amt, service_item);
+        }
+        public void clearBillingStatement(int user_id)
+        {
+            _provider.deleteRecords(user_id);
+        }
+        public void showBillingStatement(string user)
+        {
+            _provider.patient_getBillingRecords(user);
+        }
+        
 
     }
 }
